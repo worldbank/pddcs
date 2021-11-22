d88b89e412b50118a129324238957537c7a806bd
Aleksander Eilertsen <aeilertsen@worldbank.org>
2021-11-22 04:12:56 -0500

    Add read_dcs()
    Add first iteration of a function to read data from DCS.

---
+++ R/read_dcs.R
@@ -0,0 +1,47 @@
+#' Read DCS
+#'
+#' Read data from DCS.
+#'
+#' `r lifecycle::badge("experimental")`
+#'
+#' @param path character: Path to file.
+#' @return tibble
+#' @examples
+#' \dontrun{
+#' path <- "Data Nurses and Midwives in DCS (SH.MED.NUMW.P3).xlsx"
+#' df <- read_dcs(path)
+#' }
+#' @export
+read_dcs <- function(path) {
+
+  # Read data
+  df <- readxl::read_xlsx(path)
+
+  # Standardize format
+  df <- standardize_dcs(df)
+
+  return(df)
+}
+
+#' standardize_dcs
+#' @param df data.frame: A data frame with data from the WDI API.
+#' @return data.frame
+#' @noRd
+standardize_dcs <- function(df) {
+
+  # Select columns
+  df <- df[c("Country", "Time", "Series", "Data")]
+  names(df) <- c("iso3c", "year", "indicator", "value")
+  df$year <- sub("YR", "", df$year)
+
+  # Add source column
+  df$source <- "dcs"
+
+  # General standardization
+  df <- standardize_all(df)
+
+  # Convert to tibble
+  df <- dplyr::tibble(df)
+
+  return(df)
+}
